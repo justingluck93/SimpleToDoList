825f459df1afaff7e26417aa485b4b72cffde394
Justin Gluck <gluckcallahan@Justins-Mac-mini.local>
2019-02-06 20:25:36 -0500

    Creating Repo

---
+++ SimpleToDoList/SimpleToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleToDoList
+//
+//  Created by Justin Gluck on 2/6/19.
+//  Copyright © 2019 Justin Gluck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
